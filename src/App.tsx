cd9614e176be923414e5e32f64570f049d0ab9ee
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-03-30 16:43:15 -0400

    Remove useless boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import data from './data/location-matters-data.json';
+import text from './data/lm-text.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>poo</div>;
 }
 
 export default App;
