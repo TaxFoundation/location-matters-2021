f4e3010316c70cbb019a6430305a30f7455ed3bf
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-03-30 16:42:23 -0400

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'react/jsx-uses-vars': 2,
+    'react/jsx-uses-react': 2,
+  },
+};
