a9ace15f65ff0899e64e65be9470dea49ac086bb
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-03-31 10:38:49 -0400

    Bar chart draws perfectly but without axes

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -2,38 +2,68 @@ import React from 'react';
 import { axisLeft, axisBottom } from 'd3-axis';
 import { scaleLinear, scaleBand, ScaleBand, ScaleLinear } from 'd3-scale';
 
+import firmTypes from '../data/firm-types.json';
+
+const dimensions = {
+  width: 800,
+  height: 500,
+};
+
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 20,
+};
+
+const firmScale: ScaleBand<string> = scaleBand();
+firmScale.domain(firmTypes);
+firmScale.range([0, dimensions.width - margin.left - margin.right]);
+firmScale.round(true);
+firmScale.padding(0.2);
+
+const oldAndNewScale: ScaleBand<string> = scaleBand();
+oldAndNewScale.domain(['Old', 'New']);
+oldAndNewScale.range([0, firmScale.bandwidth()]);
+oldAndNewScale.round(true);
+oldAndNewScale.padding(0.1);
+
+const yScale: ScaleLinear<number, number> = scaleLinear();
+yScale.domain([0, 0.35]);
+yScale.range([0, dimensions.height - margin.top - margin.bottom]);
+
 const Bars: React.VFC<{
   rates: Rates;
-  xScale: number | undefined;
-  yScale: Function;
-  bottom: number;
-}> = ({ rates, xScale, yScale, bottom }) => {
+  type: string;
+}> = ({ rates, type }) => {
+  const bottom: number = dimensions.height - margin.top - margin.bottom;
+
   return (
     <g>
       <rect
         fill="#b81515"
-        x={xScale}
+        x={oldAndNewScale(type)}
         y={bottom - yScale(rates.ui)}
-        width={100}
+        width={oldAndNewScale.bandwidth()}
         height={yScale(rates.ui)}
       ></rect>
       <rect
         fill="#23a089"
-        x={xScale}
+        x={oldAndNewScale(type)}
         y={bottom - yScale(rates.ui) - yScale(rates.s)}
-        width={100}
+        width={oldAndNewScale.bandwidth()}
         height={yScale(rates.s)}
       ></rect>
       <rect
         fill="#6711a0"
-        x={xScale}
+        x={oldAndNewScale(type)}
         y={bottom - yScale(rates.ui) - yScale(rates.s) - yScale(rates.p)}
-        width={100}
+        width={oldAndNewScale.bandwidth()}
         height={yScale(rates.p)}
       ></rect>
       <rect
         fill="#d2cd3a"
-        x={xScale}
+        x={oldAndNewScale(type)}
         y={
           bottom -
           yScale(rates.ui) -
@@ -41,36 +71,26 @@ const Bars: React.VFC<{
           yScale(rates.p) -
           yScale(rates.i)
         }
-        width={100}
+        width={oldAndNewScale.bandwidth()}
         height={yScale(rates.i)}
       ></rect>
     </g>
   );
 };
 
+const BarGroup: React.VFC<{
+  firm: Firm;
+}> = ({ firm }) => {
+  return (
+    <g transform={`translate(${firmScale(firm.name)}, 0)`}>
+      <Bars rates={firm.old} type="Old" />
+      <Bars rates={firm.new} type="New" />
+    </g>
+  );
+};
+
 const BarChart: React.VFC<{ firms: Firm[] }> = ({ firms }) => {
-  const dimensions = {
-    width: 800,
-    height: 500,
-  };
-  const margin = {
-    top: 20,
-    right: 20,
-    bottom: 20,
-    left: 20,
-  };
-
   const firmNames: string[] = firms.map(f => f.name);
-
-  const xScale: ScaleBand<string> = scaleBand();
-  xScale.domain(firmNames);
-  xScale.range([0, dimensions.width - margin.left - margin.right]);
-  xScale.round(true);
-  xScale.padding(0.2);
-
-  const yScale: ScaleLinear<number, number> = scaleLinear();
-  yScale.domain([0, 0.35]);
-  yScale.range([0, dimensions.height - margin.top - margin.bottom]);
 
   return (
     <div>
@@ -79,20 +99,7 @@ const BarChart: React.VFC<{ firms: Firm[] }> = ({ firms }) => {
         <g id="left-axis"></g>
         <g id="bottom-axis"></g>
         {firms.map(firm => (
-          <g key={`firm-${firm.name}`}>
-            <Bars
-              rates={firm.old}
-              xScale={xScale(firm.name)}
-              yScale={yScale}
-              bottom={dimensions.height - margin.top - margin.bottom}
-            ></Bars>
-            <Bars
-              rates={firm.new}
-              xScale={xScale(firm.name)}
-              yScale={yScale}
-              bottom={dimensions.height - margin.top - margin.bottom}
-            ></Bars>
-          </g>
+          <BarGroup key={`firm-${firm.name}`} firm={firm} />
         ))}
       </svg>
     </div>
