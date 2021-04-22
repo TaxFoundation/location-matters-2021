1db3a50306bd720536ca2334a06dbe767a1fd713
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-04-22 09:27:12 -0400

    Add pym files

---
+++ public/pymSetup.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+  const pymParent = new pym.Parent(
+    'location-matters-interactive',
+    'https://location-matters-2021.netlify.app/index.html',
+    {},
+  );
+});
